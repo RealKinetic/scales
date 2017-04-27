4e85276e098b9be91ee7f1d6662fa3e701f8278c
Dustin Hiatt <dustin.hiatt@rkinetic.com>
2017-04-27 13:02:21 -0500

    Add pypi files.

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+# Copyright 2017 Real Kinetic, LLC. All Rights Reserved.
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+
+setup(
+    name='scales',
+    packages=find_packages(),
+    version='',
+    description='A library with some selection and weighted choice algorithms.',
+    author='Real Kinetic',
+    author_email='dev@realkinetic.com',
+    url='https://github.com/RealKinetic/scales',
+    download_url='',
+    keywords=['weighted', 'random', 'choice'],
+    classifiers=[],
+)
